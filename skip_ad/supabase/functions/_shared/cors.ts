4faf29a007f2ea3f66fcb7d7caf17f7e6f581c6d
刘鑫宇 <2993584083@qq.com>
2026-04-21 21:12:25 +0800

    删除误传文件

--- skip_ad/supabase/functions/_shared/cors.ts
+++
@@ -1,7 +0,0 @@
-export function createCorsHeaders(origin?: string) {
-  return {
-    'Access-Control-Allow-Origin': origin || '*',
-    'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
-    'Access-Control-Allow-Methods': 'POST, OPTIONS',
-  };
-}
